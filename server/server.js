be5f1489815c0c2526d078de2a494db0742cce03
timebasher <johannes.walenta@googlemail.com>
2018-04-24 20:48:05 +0200

    add GET /users/me + Authenticate

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ var { ObjectID } = require('mongodb');
 var { mongoose } = require('./db/mogoose');
 var { Todo } = require('./models/todo');
 var { User } = require('./models/user');
+var { authenticate } = require('./middleware/authenticate');
 
 //Init express
 var app = express();
@@ -113,16 +114,20 @@ app.patch('/todos/:id', (req, res) => {
 app.post('/user', (req, res) => {
     var body = _.pick(req.body, ['email', 'password']);
     var user = new User(body);
-  
-    user.save().then(() => {
-      return user.generateAuthToken();
-    }).then((token) => {
-      res.header('x-auth', token).send(user);
-    }).catch((e) => {
-      res.status(400).send(e);
-    })
-  });
 
+    user.save().then(() => {
+        return user.generateAuthToken();
+    }).then((token) => {
+        res.header('x-auth', token).send(user);
+    }).catch((e) => {
+        res.status(400).send(e);
+    })
+});
+
+
+app.get('/users/me', authenticate, (req, res) => {
+    res.send(req.user);
+});
 
 
 app.listen(port, () => {
